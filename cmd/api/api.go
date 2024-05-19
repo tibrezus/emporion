d669515876a6f0f383a425598b41f21e8d22c703
Tiberiu Rezus <tiberiu.rezus@gmail.com>
2024-05-19 20:27:06 +0200

    Register HTTP server

---
+++ cmd/api/api.go
@@ -0,0 +1,30 @@
+package api
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type APIServer struct {
+	addr string
+	db   *sql.DB
+}
+
+func NewAPIServer(addr string, db *sql.DB) *APIServer {
+	return &APIServer{
+		addr: addr,
+		db:   db,
+	}
+}
+
+func (s *APIServer) Run() error {
+	router := mux.NewRouter()
+	subrouter := router.PathPrefix("/api/v1").Subrouter()
+
+	userHandler := user.NewHandler()
+	userHandler.RegisterRoutes(subrouter)
+
+	return http.ListenAndServe(s.addr, router)
+}
---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/tibrezus/emporion
+
+go 1.22.3
+
+require k8s.io/apiserver v0.30.1
+
+require (
+	github.com/go-logr/logr v1.4.1 // indirect
+	github.com/gorilla/mux v1.8.1
+	k8s.io/apimachinery v0.30.1 // indirect
+	k8s.io/klog/v2 v2.120.1 // indirect
+)
