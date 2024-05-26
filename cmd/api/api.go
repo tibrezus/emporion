c7c40bbb8ec82463d80326e85d692b49f04c4fe3
Tiberiu Rezus <tiberiu.rezus@gmail.com>
2024-05-26 23:44:51 +0200

    chore: Add routes for user, product, and cart handlers

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -24,8 +24,20 @@ func (s *APIServer) Run() error {
 	router := mux.NewRouter()
 	subrouter := router.PathPrefix("/api/v1").Subrouter()
 
+	userStore := user.NewStore(s.db)
 	userHandler := user.NewHandler()
 	userHandler.RegisterRoutes(subrouter)
+
+	productStore := product.NewStore(s.db)
+	productHandler := product.NewHandler(productStore, userStore)
+	productHandler.RegisterRoutes(subrouter)
+
+	orderStore := order.NewStore(s.db)
+
+	cartHandler := cart.NewHandler(productStore, orderStore, userStore)
+	cartHandler.RegisterRoutes(subrouter)
+
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir("static"))
 
 	return http.ListenAndServe(s.addr, router)
 }
